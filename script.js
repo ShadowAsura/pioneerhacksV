799e69d2aad84d993c5460c8011b06f7654d807f
Sriram Loganathan <76449821+ShadowAsura@users.noreply.github.com>
2024-02-17 16:13:59 -0800

    Chatbot Frontend
    - Added chatbot ui
    - onbarding fixes
    -realignement

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('openChatBtn').addEventListener('click', function() {
+    document.getElementById('chatContainer').style.display = 'block';
+  });
+  
+  document.getElementById('closeChatBtn').addEventListener('click', function() {
+    document.getElementById('chatContainer').style.display = 'none';
+  });
+  
